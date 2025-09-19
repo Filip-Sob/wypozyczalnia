1bed51bd7003b37802294421aaefd3763408362f
Karbowski Kacper <karbowski_kacper@o2.pl>
2025-09-19 21:14:27 +0200

    cleanup: remove backend files, keep frontend only

--- src/main/java/pl/sobczak/wypozyczalnia/repository/DeviceRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.sobczak.wypozyczalnia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sobczak.wypozyczalnia.model.Device;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {
-}
--- src/main/java/pl/sobczak/wypozyczalnia/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.sobczak.wypozyczalnia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sobczak.wypozyczalnia.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
